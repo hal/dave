ff85aa4b965984d2e8c78b19f3786f3615f6c52f
Harald Pehl <harald.pehl@gmail.com>
2026-05-20 14:19:26 +0200

    fix: format index.html and allurerc.mjs for Prettier compliance

--- allurerc.mjs
+++ allurerc.mjs
@@ -2,18 +2,15 @@ export default {
   environments: {
     chromium: {
       name: "Chromium",
-      matcher: ({ labels }) =>
-        labels.some(({ name, value }) => name === "parentSuite" && value === "chromium"),
+      matcher: ({ labels }) => labels.some(({ name, value }) => name === "parentSuite" && value === "chromium"),
     },
     firefox: {
       name: "Firefox",
-      matcher: ({ labels }) =>
-        labels.some(({ name, value }) => name === "parentSuite" && value === "firefox"),
+      matcher: ({ labels }) => labels.some(({ name, value }) => name === "parentSuite" && value === "firefox"),
     },
     webkit: {
       name: "WebKit",
-      matcher: ({ labels }) =>
-        labels.some(({ name, value }) => name === "parentSuite" && value === "webkit"),
+      matcher: ({ labels }) => labels.some(({ name, value }) => name === "parentSuite" && value === "webkit"),
     },
   },
 };
